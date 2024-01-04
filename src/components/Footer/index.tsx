6b1c2a9ce921e742c53806cb7227bfd18b464f8a
ma <501847822@qq.com>
2024-01-04 23:52:48 +0800

    卷聚用户管理中心--初始化代码

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,33 @@
+import { GithubOutlined } from '@ant-design/icons';
+import { DefaultFooter } from '@ant-design/pro-components';
+const Footer: React.FC = () => {
+  const defaultMessage = '卷聚小分队';
+  const currentYear = new Date().getFullYear();
+  return (
+    <DefaultFooter
+      copyright={`${currentYear} ${defaultMessage}`}
+      links={[
+        {
+          key: '太原理工大学',
+          title: '太原理工大学',
+          href: 'https://www.tyut.edu.cn/',
+          blankTarget: true,
+        },
+
+        {
+          key: 'github',
+          title: <GithubOutlined />,
+          href: 'https://github.com/wenyinmahua',
+          blankTarget: true,
+        },
+        {
+          key: '太原理工大学软件学院',
+          title: '太原理工大学软件学院',
+          href: 'https://rjxy.tyut.edu.cn/',
+          blankTarget: true,
+        },
+      ]}
+    />
+  );
+};
+export default Footer;
